3b8ef8d05bd1675910d4f9204e16cfe939efa745
liu9jian <lj5069214@163.com>
2018-07-11 22:08:44 +0800

    add new pro

---
+++ soho/src/main/java/com/soho/soho/SohoApplication.java
@@ -0,0 +1,12 @@
+package com.soho.soho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SohoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SohoApplication.class, args);
+	}
+}
---
+++ soho/src/test/java/com/soho/soho/SohoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soho.soho;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SohoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
